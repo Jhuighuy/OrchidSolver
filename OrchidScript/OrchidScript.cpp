18b5f53d63fe34488abe5821724ab0a648c176d9
Oleg Butakov <butakovoleg@gmail.com>
2019-04-24 00:11:15 +0300

    Simple LibFFI example implemented

--- OrchidScript/OrchidScript.cpp
+++ OrchidScript/OrchidScript.cpp
@@ -33,8 +33,32 @@ a;
     printf("%s\n", a.operator std::string().c_str());
 }
 
+void ffftest1(int* a) 
+{
+    printf("%lld\n", *a);
+}
+
+extern "C" void ffftest();
+
+#include <ffi/ffi.h>
+#include <dlfcn.h>
 int main() 
 {
+    auto self = dlopen(nullptr, RTLD_LAZY);
+    auto self_sin = (double(*)(double))dlsym(self, "sin");
+    printf("%lf\n", self_sin(3));
+
+    ffi_type* arg_types[] = {&ffi_type_pointer};
+    ffi_cif cif;
+    ffi_prep_cif(&cif, FFI_DEFAULT_ABI, 1, &ffi_type_void, arg_types);
+    int a = 1488;
+    void* pa = &a;
+    void* ppa = &pa;
+    int b;
+    //printf("%lld\n", ffftest);
+    ffi_arg rc;
+    ffi_call(&cif, FFI_FN(ffftest), &rc, &ppa);
+    
     orchid_solver_scanner_test();
     return 0;
 }
